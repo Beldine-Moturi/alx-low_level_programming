5ebc5b389e2b4bb70be9176b59d2cad4df19b5dc
Beldine-Moturi <beldinemoturi@gmail.com>
2021-08-31 11:30:57 -0700

    print char to stdout

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *main - prints the characters "Holberton to stdout"
+ *
+ *Return: returns 0
+ */
+
+int main(void)
+{
+	int i;
+
+	char *c = "Holberton";
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# C Functions and nested loops
+
+The files in this repo are c programs that utilise functions
