542d25c8aea6e2f85858ffc99196557b67897478
Beldine-Moturi <76222648+Beldine-Moturi@users.noreply.github.com>
2021-09-02 14:59:59 +0300

    add space after first statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	i = n % 10;
 
-	printf("Last digit of %d is", n);
+	printf("Last digit of %d is ", n);
 
 	if (i > 5)
 		printf("%d and is greater than 5\n", i);
