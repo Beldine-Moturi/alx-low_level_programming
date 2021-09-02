3f0ae81ebfa9744b33edd4748b7f0e676a6d827e
Beldine-Moturi <76222648+Beldine-Moturi@users.noreply.github.com>
2021-09-02 15:01:40 +0300

    remove colon

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		printf("%d: is positive", n);
+		printf("%d is positive", n);
 	else if (n < 0)
-		printf("%d: is negative", n);
+		printf("%d is negative", n);
 	else
-		printf("%d: is zero", n);
+		printf("%d is zero", n);
 	printf("\n");
 
 	return (0);
