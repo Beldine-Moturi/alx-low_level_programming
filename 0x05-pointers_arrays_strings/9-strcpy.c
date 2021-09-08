2b80e92a40ec5e6683068009a39c7da7e60ae795
Beldine-Moturi <beldinemoturi@gmail.com>
2021-09-08 09:42:06 -0700

    remove merge conflict markers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,6 @@
 #include "main.h"
 
 /**
- *_strcpy - copies the string
- * @dest: pointer to the string
- * @src: string to be copied
- *
- * Return: return pointer
- */
-char *_strcpy(char *dest, char *src)
-{
-	int len, i;
-
-	len = 0;
-
-	while (src[len] != '\0')
-=======
  *_strcpy - copies a string to a buffer
  *@dest: the buffer pointed to
  *@src: the string tp copy
