c91294da9771c33ccab43e60b8d6e148c8690ee8
Beldine-Moturi <beldinemoturi@gmail.com>
2021-09-08 07:38:13 -0700

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *rev_string - reverses a string
+ *@s: the string to be reversed
+ *
+ *Return: has no return value
+ *
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, i;
+
+	char *start, *end, temp;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+
+	start = s;
+	end = s;
+
+	for (i = 0; i < len - 1; i++)
+	{
+		end++;
+	}
+
+	for (i = 0; i <= len / 2; i++)
+	{
+		temp = *start;
+		*start = *end;
+		*end = temp;
+
+		start++;
+		end--;
+	}
+}
