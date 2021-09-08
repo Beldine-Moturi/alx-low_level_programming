0bf5461e65ce461da4ee288cfafc365872183897
Beldine-Moturi <beldinemoturi@gmail.com>
2021-09-08 06:37:47 -0700

    print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_rev - prints a string, in reverse
+ *@s: the string to be printed
+ *
+ *Return: has no return value
+ *
+ */
+
+void print_rev(char *s)
+{
+	int len, i;
+
+	for (len = 0; *(s + len) != '\0'; len++)
+	{
+		len++;
+	}
+	for (i = (len - 1); i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
