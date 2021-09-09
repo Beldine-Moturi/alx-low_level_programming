f25208a31fa5cf11f783f0517751a904dd497c65
Beldine-Moturi <beldinemoturi@gmail.com>
2021-09-09 07:51:15 -0700

    update to file 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,13 @@
 
 void print_rev(char *s)
 {
-	int len, i;
+	int i, len = 0;
 
-	for (len = 0; s[i] != '\0'; len++)
+	while (s[len] != '\0')
 	{
 		len++;
 	}
+
 	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
