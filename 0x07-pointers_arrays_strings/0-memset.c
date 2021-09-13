7e2e2f3c2e0435f5f9c37ffe01e92545746e3b7b
Beldine-Moturi <76222648+Beldine-Moturi@users.noreply.github.com>
2021-09-13 22:47:50 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -15,10 +15,9 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
-		if (i < n)
-			s[i] = b;
+		*(s + i) = b;
 	}
 	return (s);
 }
