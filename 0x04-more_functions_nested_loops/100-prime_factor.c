1075afaa1d7630c7b9a6487ab26147f745aba66c
Beldine-Moturi <beldinemoturi@gmail.com>
2021-09-13 05:49:20 -0700

    find and print largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - finds and prints the largets prime factor of n
+ *
+ *Return: returns 0 on success
+ *
+ */
+
+int main(void)
+{
+	long i, number = 612852475143;
+
+	for (i = 2; i <= 612852475143; i++)
+	{
+		if (number % i == 0)
+		{
+			number = number / i;
+			i--;
+		}
+	}
+	printf("%lu\n", i);
+
+	return (0);
+}
