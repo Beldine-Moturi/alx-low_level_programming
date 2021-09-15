950b4ee610ff14399ea3dbf04910a87dbdce8912
Beldine-Moturi <beldinemoturi@gmail.com>
2021-09-15 10:27:52 -0700

    correct code

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+* isSqrt - helper to _sqrt_recursion
+* @n: input number
+* @i: iterator
+*
+* Return: square root of number
+*/
+
+int isSqrt(int n, int i)
+{
+if ((i * i) > n)
+{
+return (-1);
+}
+if ((i * i) == n)
+{
+return (i);
+}
+return (isSqrt(n, i + 1));
+}
+
+/**
 * _sqrt_recursion - returns natural root of +ve int
 * @n: input number
 *
