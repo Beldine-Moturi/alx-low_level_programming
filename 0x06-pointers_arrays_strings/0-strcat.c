83658b465f5f7b5db34b4b043c1d826c09bf9e7a
Beldine-Moturi <beldinemoturi@gmail.com>
2021-09-15 11:51:55 -0700

    concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,5 +12,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	
-}	
+	int len1 = 0, len2 = 0;
+
+	while (dest[len1] != '\0')
+	{
+		len1++;
+	}
+	while (src[len2] != '0')
+	{
+		dest[len1] = src[len2];
+		len2++;
+		len1++;
+	}
+	dest[len1] = '\0';
+
+	return (dest);
+}
