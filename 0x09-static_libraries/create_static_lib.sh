b1f03405bd0d80e52007bd3aed8afbc64d1851ee
Beldine-Moturi <beldinemoturi@gmail.com>
2021-09-20 06:02:31 -0700

    try different code

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c | ar -rc liball.a | ranlib
