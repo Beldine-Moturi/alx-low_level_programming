58abbbb4781bb76525b095573627868c8d0f1da9
Beldine-Moturi <beldinemoturi@gmail.com>
2021-09-24 09:29:39 -0700

    correction

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,7 +13,7 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, sum1, sum2;
+	int i, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
@@ -23,7 +23,7 @@ void print_diagsums(int *a, int size)
 
 	a -= size;
 
-	for (i = 0; i < size; i++)
+	for (i = 0;  < size; i++)
 	{
 		sum2 += a[i];
 		a -= size;
