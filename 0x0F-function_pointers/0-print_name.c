7a64267def36144b50264977ba11839b9386cd32
Beldine-Moturi <beldinemoturi@gmail.com>
2021-09-29 15:57:31 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - prints a name passed to it as argument
+ *@name: the name to be printed
+ *@f: the function that prints the name
+ *
+ *Return: void - has no return value
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,8 @@
+# C-Function pointers
+
+The files in this directory are functions that implemement function pointers in C to solve various problems
+
+
+## 0-print_name.c
+
+This function prints a name passed to it as argument
