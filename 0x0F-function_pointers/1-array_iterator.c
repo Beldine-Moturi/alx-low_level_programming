cc8360accb741e31448aac8166f27a1992ebcfad
Beldine-Moturi <beldinemoturi@gmail.com>
2021-09-29 16:20:48 -0700

    correction

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -13,6 +14,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
+
+	if (array == NULL || size == 0 || action == NULL)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
