23bed797d5ebe4a3fff4fc51e4363b70966d26f9
Beldine-Moturi <beldinemoturi@gmail.com>
2021-10-04 22:06:08 -0700

    use printf

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -26,7 +26,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i != (n - 1) && separator != NULL)
 			printf("%c", *separator);
 	}
-	_putchar('\n');
+	printf("\n");
 
 	va_end(num);
 }
