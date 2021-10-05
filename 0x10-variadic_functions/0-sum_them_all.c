cc56eda6cca7351d1081b969471018b29c94a970
Beldine-Moturi <beldinemoturi@gmail.com>
2021-10-04 21:22:47 -0700

    return sum of parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,29 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 
 /**
- * sum_them_all - Returns the sum of all its paramters.
- * @n: The number of paramters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
+ * sum_them_all - calculates and returns the sum of all its parameters
+ *@n: count of the number of parameters passed
+ *@...: a variable number of parameters to calculate the sum of
  *
- * Return: If n == 0 - 0.
- *         Otherwise - the sum of all parameters.
+ *Return: sum of all the parameters
+ *        or 0 if n == 0
+ *
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int index, sum = 0;
+	unsigned int i = 0, sum = 0;
+	va_list num;
 
-	va_start(nums, n);
+	if (n == 0)
+		return (0);
 
-	for (index = 0; index < n; index++)
-		sum += va_arg(nums, int);
-
-	va_end(nums);
-
+	va_start(num, n);
+	while (i < n)
+	{
+		sum += va_arg(num, int);
+		i++;
+	}
+	va_end(num);
 	return (sum);
 }
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,1 +1,28 @@
-C-Variadic Functions
+#C-Variadic Functions
+
+This directory is a collection of variadic functions used to solve various problems
+
+
+## 0-sum_them_all.c
+
+This function returns the sum of all its variable parameters
+
+
+## 1-print_numbers.c
+
+Tis function prints variable numbers, followed by a new line
+
+
+## 2-print_strings.c
+
+This function prints a variable number of strings passed as arguments
+
+
+## 3-print_all.c
+
+This function prints anything passed onto it as argument
+
+
+## variadic_functions.h
+
+This is a header file that contails all the prototypes of the functions in this directory as well as declarations of strucutres and macros used in these files
