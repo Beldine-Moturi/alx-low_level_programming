ac014125ec8ddf63f009c5dc48a13e01ab7bbd2d
Beldine-Moturi <beldinemoturi@gmail.com>
2021-10-04 22:12:12 -0700

    correction

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,7 +25,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(num, int));
 
 		if (i != (n - 1) && separator != NULL)
-			printf("%s", *separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 
