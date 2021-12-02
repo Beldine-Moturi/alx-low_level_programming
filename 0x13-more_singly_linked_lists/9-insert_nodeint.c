5703ea5a36da89b0294bdb6463c3ae842a36b058
Beldine-Moturi <beldinemoturi@gmail.com>
2021-12-02 19:07:55 +0300

    -corrections

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new->n = n;
 	temp = *head;
-	if (temp == NULL)
+	if (temp == NULL || temp->next == NULL)
 		return (NULL);
 	while (temp != NULL)
 	{
