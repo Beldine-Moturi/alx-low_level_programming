2e652c4e513300f97b3d9e8816c4e438a0d8dffc
Beldine-Moturi <beldinemoturi@gmail.com>
2021-12-21 12:32:47 +0300

    return NULL for ht NULL

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,6 +15,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (index >= ht->size)
 		return (NULL);
