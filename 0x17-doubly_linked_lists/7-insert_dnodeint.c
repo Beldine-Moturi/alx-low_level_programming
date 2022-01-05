c851a37f60645e4daca162721cd8c0f7a973eec2
Beldine-Moturi <beldinemoturi@gmail.com>
2022-01-05 18:14:13 +0300

    fix bugs

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,26 +12,29 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp, *new;
-	unsigned int count = 0;
+	dlistint_t *temp = *h, *new;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+	}
+
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	new->n = n;
-	temp = *h;
 
-	while (temp != NULL && count != idx)
-	{
-		temp = temp->next;
-		count++;
-	}
-	if (temp != NULL)
-	{
-		new->next = temp;
-		new->prev = temp->prev;
-		(temp->prev)->next = new;
-		temp->prev = new;
-	}
-	return (temp);
+	new->n = n;
+	new->next = temp->next;
+	new->prev = temp;
+	(temp->next)->prev = new;
+	temp->next = new;
+	return (new);
 }
